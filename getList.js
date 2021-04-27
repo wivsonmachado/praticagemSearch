2cff0cf44cd9f8c6ff60088c5afc5b27933e6263
Wivson Machado <wivsonfrota@gmail.com>
2021-04-27 11:27:43 -0300

    Separando o código em modulos

---
+++ getList.js
@@ -0,0 +1,44 @@
+import cheerio from 'cheerio'
+import navio from './objRaw.js'
+
+const getLista = html => {
+    const arrayNavios = []
+    var $ = cheerio.load(html)
+            $('.quadro')
+                .eq(0)
+                .find('table')
+                .eq(2)
+                .find('tr')
+                .each(function(i){
+                    var dataHora = $(this)
+                            .find('td')
+                            .eq(0)
+                            .text()
+                            .trim()
+                    var nomeNavio = $(this)
+                            .find('td')
+                            .eq(1)
+                            .text()
+                            .trim()
+                    var manobra = $(this)
+                            .find('td')
+                            .eq(6)
+                            .text()
+                            .trim()
+                    var de = $(this)
+                            .find('td')
+                            .eq(7)
+                            .text()
+                            .trim()
+                    var para = $(this)
+                            .find('td')
+                            .eq(8)
+                            .text()
+                            .trim()
+                    arrayNavios.push(new navio(dataHora, nomeNavio, manobra, de, para))                
+                })
+    console.log(arrayNavios)
+    //return arrayNavios           
+}
+
+export default getLista
---
+++ objRaw.js
@@ -0,0 +1,9 @@
+function navio(dataHora, nomeNavio, manobra, de, para){
+    this.dataHora = dataHora
+    this.nomeNavio = nomeNavio
+    this.manobra = manobra
+    this.de = de
+    this.para = para
+}
+
+export default navio
