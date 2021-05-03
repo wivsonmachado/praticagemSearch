c3bff6328ba6b12c9c8c161497679c6a4c85d10d
Wivson Machado <wivsonfrota@gmail.com>
2021-05-03 13:47:17 -0300

    findVessel merged de testes para master com correções do Case 2

--- findVessel.js
+++ findVessel.js
@@ -14,27 +14,47 @@ function vessel(ship) {
                 }
             }
             if (retornaTrueOrFalse() === 1) {
-                function busca(i) {
-                    if (i.nomeNavio === ship) {
-                        
-                        switch (i.manobra) {
-                            case 'ENTRADA':
-                                //return `O navio ${i.nomeNavio} está com prático marcado para ${i.dataHora}hs, realizará uma ${i.manobra} para ${i.para}.`
-                                console.log(`O navio ${i.nomeNavio} está com prático marcado para ${i.dataHora}hs, realizará uma ${i.manobra} para ${i.para}.`)
-                                break
-                            case 'MUDANÇA':
-                                //return `O navio ${i.nomeNavio} está com prático marcado para ${i.dataHora}hs, realizará uma ${i.manobra} de ${i.de} para ${i.para}.`
-                                console.log(`O navio ${i.nomeNavio} está com prático marcado para ${i.dataHora}hs, realizará uma ${i.manobra} de ${i.de} para ${i.para}.`)
-                                break
-                            case 'SAÍDA':
-                                //return `O navio ${i.nomeNavio} está com prático marcado para ${i.dataHora}hs, realizará uma ${i.manobra} de ${i.de}.`
-                                console.log(`O navio ${i.nomeNavio} está com prático marcado para ${i.dataHora}hs, realizará uma ${i.manobra} de ${i.de}.`)
-                                break
+                const navioEncontrado = buscaIndex.filter((i) => i.nomeNavio === ship)
+                //console.log(navioEncontrado)
+                if(navioEncontrado.length > 1){
+                    let dataHoraAtual = ''
+                    navioEncontrado.forEach((i) => {
+                        if(dataHoraAtual !== i.dataHora){
+                            switch (i.manobra) {
+                                case 'ENTRADA':
+                                    //return `O navio ${i.nomeNavio} está com prático marcado para ${i.dataHora}hs, realizará uma ${i.manobra} para ${i.para}.`
+                                    console.log(`O navio ${i.nomeNavio} está com prático marcado para ${i.dataHora}hs, realizará uma ${i.manobra} para ${i.para}.`)
+                                    break
+                                case 'MUDANÇA':
+                                    //return `O navio ${i.nomeNavio} está com prático marcado para ${i.dataHora}hs, realizará uma ${i.manobra} de ${i.de} para ${i.para}.`
+                                    console.log(`O navio ${i.nomeNavio} está com prático marcado para ${i.dataHora}hs, realizará uma ${i.manobra} de ${i.de} para ${i.para}.`)
+                                    break
+                                case 'SAÍDA':
+                                    //return `O navio ${i.nomeNavio} está com prático marcado para ${i.dataHora}hs, realizará uma ${i.manobra} de ${i.de}.`
+                                    console.log(`O navio ${i.nomeNavio} está com prático marcado para ${i.dataHora}hs, realizará uma ${i.manobra} de ${i.de}.`)
+                                    break
+                            }                           
                         }
-                        
+                        dataHoraAtual = i.dataHora
+                    })
+                }else{
+                    navioEncontrado.forEach((i) => {
+                        switch (i.manobra) {
+                        case 'ENTRADA':
+                            //return `O navio ${i.nomeNavio} está com prático marcado para ${i.dataHora}hs, realizará uma ${i.manobra} para ${i.para}.`
+                            console.log(`O navio ${i.nomeNavio} está com prático marcado para ${i.dataHora}hs, realizará uma ${i.manobra} para ${i.para}.`)
+                            break
+                        case 'MUDANÇA':
+                            //return `O navio ${i.nomeNavio} está com prático marcado para ${i.dataHora}hs, realizará uma ${i.manobra} de ${i.de} para ${i.para}.`
+                            console.log(`O navio ${i.nomeNavio} está com prático marcado para ${i.dataHora}hs, realizará uma ${i.manobra} de ${i.de} para ${i.para}.`)
+                            break
+                        case 'SAÍDA':
+                            //return `O navio ${i.nomeNavio} está com prático marcado para ${i.dataHora}hs, realizará uma ${i.manobra} de ${i.de}.`
+                            console.log(`O navio ${i.nomeNavio} está com prático marcado para ${i.dataHora}hs, realizará uma ${i.manobra} de ${i.de}.`)
+                            break
                     }
+                    })
                 }
-                buscaIndex.forEach(busca)
             } else {
                 //return'Navio não encontrado'
                 console.log('Navio não encontrado')
@@ -43,4 +63,7 @@ function vessel(ship) {
 
 }
 
-vessel('MSC SOFIA CELESTE')
+vessel('BBC AQUAMARINE') // 1 vez só, retorna 1
+vessel('NORTHERN MAJESTIC') // 4 vezes, retorna 2
+vessel('UASC ZAMZAM') // 2 vezes, retorna 1
+vessel('LOG-IN PANTANAL') // 2 vezes, retorna 2
