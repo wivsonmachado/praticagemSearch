81fbe0c10b11a98b16911d57899c2908a6da0c30
Wivson Machado <wivsonfrota@gmail.com>
2021-05-05 14:04:37 -0300

    Revert "instalando e configurando a API com CORS"
    This reverts commit 545360051f29354b0321a52f2e039594d543db23.

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ import getLista from './getList.js'*/
 const express = require('express')
 const htmlData = require('./htmlData.js')
 const getLista = require('./getList.js')
-const cors = require('cors')
 
 const app = express()
 
-app.get('/json', cors(), async (req, res) =>{
-    res.setHeader()
+app.get('/json', async (req, res) =>{
     res.json(getLista(await htmlData()))
 })
 
