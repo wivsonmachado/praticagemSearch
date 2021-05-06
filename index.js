01b596746e0da019eecd40906ca937a393fb64e8
Wivson Machado <wivsonfrota@gmail.com>
2021-05-06 15:19:45 -0300

    reorgarnizada a estrutura com arquivos EJS e implementada a renderização do index.ejs no app

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ const express = require('express')
 const htmlData = require('./htmlData.js')
 const getLista = require('./getList.js')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 
+app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, "public")))
 app.use(cors())
 
 app.get('/json', async (req, res) =>{
     res.json(getLista(await htmlData()))
+})
+
+app.get('/', (req, res)=>{
+    res.render('index')
 })
 
 app.listen(3000, ()=>{
