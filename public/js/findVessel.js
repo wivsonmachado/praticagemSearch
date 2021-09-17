60e01ae084d4213e7da6ddb73660ed3f34dbd604
Wivson Machado <wivsonfrota@gmail.com>
2021-09-17 13:37:29 -0300

    implementada verificação condicional para input vazio

--- public/js/findVessel.js
+++ public/js/findVessel.js
@@ -38,62 +38,66 @@ function returnSwitch(i){
 
 
 function vessel(shipName) {
-    let divRes = document.querySelector('#res')
-    divRes.innerHTML = '<div class="spinner-border text-danger" role="status"><span class="sr-only">Searching...</span></div>'
-    let divTotal = document.getElementsByClassName('resultado').length
-    for(let i = divTotal; i > -1 ; i--){
-        let el = document.getElementsByClassName('resultado')[i]
-        if(el){
-        el.remove()
+    if(shipName === ""){
+        let divRes = document.querySelector('#res')
+        divRes.innerHTML = '<div class="spinner-border text-danger" role="status"><span class="sr-only">Searching...</span></div>'
+        let divTotal = document.getElementsByClassName('resultado').length
+        for(let i = divTotal; i > -1 ; i--){
+            let el = document.getElementsByClassName('resultado')[i]
+            if(el){
+            el.remove()
+            }
         }
-    }
-    shipName = document.querySelector('#navio').value
-    ship = shipName.toUpperCase().trim()
-    let url = 'https://shielded-springs-02765.herokuapp.com/json'
-    let buscaIndex
-    fetch(url).then((res) => res.json())
-        .then(json => {
-            divRes.innerHTML = ''
-            buscaIndex = json
-            function retornaTrueOrFalse() {
-                if (buscaIndex.findIndex(i => i.nomeNavio === ship) < 0) {
-                    return -1
+        shipName = document.querySelector('#navio').value
+        ship = shipName.toUpperCase().trim()
+        let url = 'https://shielded-springs-02765.herokuapp.com/json'
+        let buscaIndex
+        fetch(url).then((res) => res.json())
+            .then(json => {
+                divRes.innerHTML = ''
+                buscaIndex = json
+                function retornaTrueOrFalse() {
+                    if (buscaIndex.findIndex(i => i.nomeNavio === ship) < 0) {
+                        return -1
+                    } else {
+                        return 1
+                    }
+                }
+                if (retornaTrueOrFalse() === 1) {
+                    const navioEncontrado = buscaIndex.filter((i) => i.nomeNavio === ship)
+                    //console.log(navioEncontrado)
+                    if(navioEncontrado.length > 1){
+                        let dataHoraAtual = ''
+                        navioEncontrado.forEach((i) => {
+                            if(dataHoraAtual !== i.dataHora){
+                                returnSwitch(i)                        
+                            }
+                            dataHoraAtual = i.dataHora
+                        })
+                    }else{
+                        navioEncontrado.forEach((i) => {
+                            returnSwitch(i)
+                        })
+                    }
                 } else {
-                    return 1
+                    document.querySelector('#navio').value = ''
+                    let divPai = document.querySelector('#res')
+                    let divFilho = document.createElement('div')
+                    let divNeto = document.createElement('p')
+                    let texto
+                    texto = document.createTextNode(`Até o momento o navio ${ship} não foi adicionado à lista da praticagem-rj.`)
+                
+                    divFilho.className = 'resultado'
+                    divNeto.appendChild(texto)
+                    divFilho.appendChild(divNeto)
+                    divPai.appendChild(divFilho)
+                    //alert('Navio não encontrado') 
+                    //console.log('Navio não encontrado')
                 }
-            }
-            if (retornaTrueOrFalse() === 1) {
-                const navioEncontrado = buscaIndex.filter((i) => i.nomeNavio === ship)
-                //console.log(navioEncontrado)
-                if(navioEncontrado.length > 1){
-                    let dataHoraAtual = ''
-                    navioEncontrado.forEach((i) => {
-                        if(dataHoraAtual !== i.dataHora){
-                            returnSwitch(i)                        
-                        }
-                        dataHoraAtual = i.dataHora
-                    })
-                }else{
-                    navioEncontrado.forEach((i) => {
-                        returnSwitch(i)
-                    })
-                }
-            } else {
-                document.querySelector('#navio').value = ''
-                let divPai = document.querySelector('#res')
-                let divFilho = document.createElement('div')
-                let divNeto = document.createElement('p')
-                let texto
-                texto = document.createTextNode(`Até o momento o navio ${ship} não foi adicionado à lista da praticagem-rj.`)
-            
-                divFilho.className = 'resultado'
-                divNeto.appendChild(texto)
-                divFilho.appendChild(divNeto)
-                divPai.appendChild(divFilho)
-                //alert('Navio não encontrado') 
-                //console.log('Navio não encontrado')
-            }
-        }).catch((error) => console.log(error.message))
+            }).catch((error) => console.log(error.message))
+    }else{
+        alert("Insira o nome de um navio")
+    }
     
 
 }
